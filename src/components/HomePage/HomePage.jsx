4f5b806831450bfc92419372cbae270582db2df4
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2019-07-27 17:31:52 -0300

    Cria modal "Em breve"
    Esse modal exibe um texto dizendo que a funcionalidade de acessar o
    repositório não está disponível, mas que será adicionada em breve.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import css from './HomePage.module.css'
-import { Button } from 'antd'
+import { Button, Modal } from 'antd'
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isModalVisible: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      isModalVisible: true
+    })
+  }
+
+  handleModalClose() {
+    this.setState({
+      isModalVisible: false
+    })
+  }
+
   render() {
     return (
       <div className={css.heroContainer}>
@@ -10,7 +30,33 @@ class HomePage extends React.Component {
           Bem-vindo à NOME, o repositório de conhecimentos da Masti
           Education!
         </h1>
-        <Button type="primary" size="large" className={css.heroBtn}>Acessar o repositório</Button>
+
+        <Button
+          type="primary"
+          size="large"
+          className={css.heroBtn}
+          onClick={this.handleClick}
+        >Acessar o repositório</Button>
+
+        <Modal
+          title="EM BREVE!!!"
+          centered
+          footer={null}
+          visible={this.state.isModalVisible}
+          onCancel={() => this.handleModalClose()}
+          bodyStyle={{ fontSize: '16px' }}
+          wrapClassName={css.modal}
+        >
+          <p>
+            Essa funcionalidade ainda não está disponível no momento,
+            mas nosso grupo de desenvolvedores está fazendo o possível
+            para que ela esteja pronta quanto antes.
+          </p>
+          <p>
+            Só não desanime agora! Cheque outros conteúdos no nosso
+            site: <a href="https://masti.com.br/">masti.com.br</a>
+          </p>
+        </Modal>
       </div>
     )
   }
