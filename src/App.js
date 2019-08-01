561da0ff8698790e82ad46346f834f12d9f581da
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2019-08-01 20:05:20 -0300

    Cria a Dashboard e a adiciona ao roteador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import HomePage from './components/HomePage/HomePage'
+import Dashboard from './components/Dashboard/Dashboard'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={HomePage} />
+        <Route path="/" component={HomePage} exact />
+        <Route path="/dashboard" component={Dashboard} exact />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div>
+        pudim
+      </div>
+    )
+  }
+}
+
+export default Dashboard
