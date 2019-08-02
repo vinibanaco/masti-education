2d579c203735f3fd97b1a792a3757cd9c26d3c26
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2019-08-01 21:36:11 -0300

    Exibe os dados do usuário na dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
 class Dashboard extends React.Component {
+
+  /* ======== FUNCTIONS ======== */
+
+  renderGender = gender => {
+    if (gender)
+      return <p>Gênero: {gender}</p>
+    return null
+  }
+
+  renderAge = age => {
+    if (age)
+      return <p>Idade: {age}</p>
+    return null
+  }
+
+  /* ======== REACT METHODS ======== */
+
   render() {
+    const user = this.props.location.state
+
     return (
       <div>
-        pudim
+        <p>Nome: {user.name}</p>
+        <p>Email: {user.email}</p>
+        {this.renderGender(user.gender)}
+        {this.renderAge(user.age)}
       </div>
     )
   }
