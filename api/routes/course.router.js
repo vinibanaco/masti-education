6a1ccf7beefc860e5bdddaf233d159a6e38f57d5
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2019-08-17 11:49:36 -0300

    Corrige acesso ao endpoint para deslogados

--- api/routes/course.router.js
+++ api/routes/course.router.js
@@ -1,35 +1,42 @@
 const express = require('express')
+const passport = require('passport')
 const { db } = require('../database/db')
 
 const router = express.Router()
 
 /* ===== GET ALL COURSES ===== */
-router.get('/', (req, res, next) => {
-  db.query('SELECT * FROM course', (err, results) => {
-    if (err) {
-      return next(err)
-    }
-
-    if (results.length === 0) {
-      return res.status(404).send('Curso não encontrado')
-    }
-
-    res.json(results[0])
-  })
-})
-
-/* ===== GET COURSE ===== */
-router.get('/:id', (req, res, next) => {
-  db.query(
-    'SELECT * FROM course WHERE id=?',
-    [req.course.id],
-    (err, results) => {
+router.get('/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    db.query('SELECT * FROM course', (err, results) => {
       if (err) {
         return next(err)
       }
-      res.send(results)
-    }
-  )
-})
+
+      if (results.length === 0) {
+        return res.status(404).send('Curso não encontrado')
+      }
+
+      res.json(results[0])
+    })
+  }
+)
+
+/* ===== GET COURSE ===== */
+router.get('/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    db.query(
+      'SELECT * FROM course WHERE id=?',
+      [req.course.id],
+      (err, results) => {
+        if (err) {
+          return next(err)
+        }
+        res.send(results)
+      }
+    )
+  }
+)
 
 module.exports = router
