0bcb62244619d3aa6b1681bf7ff9393a0a8b8bfe
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2019-08-31 11:14:22 -0300

    Adiciona um componente de rota segura
    Esse componente verifica se existe um token, caso exista, ele exibe o
    conteúdo, senão, ele redireciona o usuário para a página inicial.

---
+++ web/src/components/SecureRoute/SecureRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Redirect } from 'react-router-dom'
+
+class SecureRoute extends React.Component {
+  render() {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      return this.props.children
+    }
+
+    return <Redirect path='/' />
+  }
+}
+
+export default SecureRoute
